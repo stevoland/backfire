c9b70b4e79c63ccf5f708f1355035cfcba74a828
stevo <stevoland@gmail.com>
2013-12-27 19:12:45 +0000

    Get working

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('./bower_components/backbone/backbone-min');
+require('./vendor/firebase');
+var Backbone = require('./bower_components/backbone/backbone');
+window.Backbone = Backbone;
+window._ = require('underscore');
 require('./bower_components/backfire/backbone-firebase');
 
-module.exports = window.Backbone.Firebase;
+module.exports = window.Backbone;
